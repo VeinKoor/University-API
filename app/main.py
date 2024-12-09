b4a3c2ce224349c40b704e1a198723a3499d056f
Sergey <onetap.cs@yandex.ru>
2024-12-09 22:37:21 +0700

    add model for students

--- app/main.py
+++ app/main.py
@@ -2,12 +2,69 @@ from fastapi import FastAPI
 from app.utils import json_to_dict_list # Функция, которая будет возвращать всех студентов
 import os # Для настройки относительных путей к JSON
 from typing import Optional, List # Передавать значения по умолчанию в параметры пути и запросов
-from pydantic import BaseModel # Для создания схем данных
-
-
+from pydantic import BaseModel, EmailStr, Field, field_validator, ValidationError # Для создания схем данных
+from enum import Enum # Для создания перечислений
+from datetime import datetime, date # Для работы с датами
+import re # Для использования регулярных выражений
 
 script_dir = os.path.dirname(os.path.abspath(__file__)) # Получаем путь к директории текущего скрипта
 path_to_json = os.path.join(script_dir, 'students.json') # Получаем путь к JSON
+
+
+# Класс для перечисления факультетов, наследование от str и Enum для создания перечисления, где каждый член является строкой
+class Major(str, Enum):
+    informatics = "Информатика"
+    economics = "Экономика"
+    law = "Право"
+    medicine = "Медицина"
+    engineering = "Инженерия"
+    languages = "Языки"
+
+
+
+# Класс для описания модели студента
+class Student(BaseModel):
+    """
+    Описание полей
+
+    Default - это то значение, которое в поле будет использоваться по умолчанию. Если мы передадим значение «…»
+    аргументом этого параметра, то это будет значить, что данное значение обязательно.
+    В связи с этим, часто указание «default» игнорируется и запись начинается с …
+    """
+
+    student_id: str
+    phone_number: str = Field(..., description='Номер телефона в международном формате, начинающийся с "+"')
+    first_name: str = Field(..., min_length=1, max_length=50, description='Имя студента, от 1 до 50 символов')
+    last_name: str = Field(..., min_length=1, max_length=50, description='Фамилия студента, от 1 до 50 символов')
+    date_of_birth: date = Field(..., description='Дата рождения студента в формате ГГГГ-ММ-ДД')
+    email: EmailStr = Field(..., description='Электронная почта студента')
+    address: str = Field(..., min_length=10, max_length=200, description='Адрес студента, не более 200 символов')
+    enrollment_year: int = Field(..., ge=2002, description='Год поступления, не меньше 2002')
+    major: Major = Field(..., description='Специальность студента')
+    course: int = Field(..., ge=1, le=5, description='Курс, в диапазоне от 1 до 5')
+    special_notes: Optional[str] = Field(..., max_length=500, description='Дополнительные заметки, не более 500 символов')
+
+
+
+    # Внутренние валидаторы
+    @field_validator('phone_number') # В декоратор передаем название поля, в котором будем проверять валидность
+    @classmethod
+    def validate_phone_number(cls, values: str) -> str: # Первым аргументом мы передаем класс, а второй аргумент - это то значение, валидность которого мы будем проверять
+        if not re.match(r'^\+\d{1,15}$', values):
+            raise ValueError('Номер телефона должен начинаться с "+" и содержать от 1 до 15 цифр')
+        return values # Возвращаем либо значение, либо ошибку
+
+
+    @field_validator("date_of_birth") # В декоратор передаем название поля, в котором будем проверять валидность
+    @classmethod
+    def validate_date_of_birth(cls, values: date): # Первым аргументом мы передаем класс, а второй аргумент - это то значение, валидность которого мы будем проверять
+        if values and values >= datetime.now().date():
+            raise ValueError('Дата рождения должна быть в прошлом')
+        return values # Возвращаем либо значение, либо ошибку
+
+
+
+
 
 app = FastAPI()
 
